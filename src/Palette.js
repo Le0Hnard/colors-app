3e075bf8c243d8491940cbb9da72a274262de7af
Jose Leonard C. Rico <jleorico963@gmail.com>
2020-10-24 18:35:56 +0800

    added palettes

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import react, { Component } from 'react';
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                <div className="Palette-colors">
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Palette;
